b689f58ee45c4718dbad81911d31be06201ea023
dean <deanraguso@gmail.com>
2021-03-21 10:52:36 +1100

    Added CRUD methods to notes conroller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,14 @@
 class NotesController < ApplicationController
     def index
-        
+        @notes = Note.all
+    end
+    def create
+        @note = Note.new(note_params)
+    end
+    def destroy
+    end
+    def updated
+    end
+    def show
     end
 end
