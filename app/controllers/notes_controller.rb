0ca3b9ac59114ac362909253532d1dd24f59a743
dean <deanraguso@gmail.com>
2021-03-21 11:50:16 +1100

    Create functional

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,13 +2,29 @@ class NotesController < ApplicationController
     def index
         @notes = Note.all
     end
+    def new
+        @note = Note.new
+    end
     def create
         @note = Note.new(note_params)
+
+        if @note.save
+            redirect_to @note
+        else
+            render 'new'
+        end
     end
     def destroy
     end
-    def updated
+    def update
     end
     def show
+        @note = Note.find(params[:id])
     end
+
+    private 
+    def note_params
+        params.require(:note).permit(:title, :body)
+    end
+    
 end
